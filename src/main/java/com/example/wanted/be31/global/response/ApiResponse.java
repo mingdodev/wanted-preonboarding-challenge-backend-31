aa78adec068e7547f50d5b06fa543b7b56eac98f
mingdodev <mingdo.dev@gmail.com>
2025-05-06 23:02:38 +0900

    feat: 공통 응답 포맷 및 전역 예외 처리 설정
    - Lombok 의존성 추가

---
+++ src/main/java/com/example/wanted/be31/global/response/ApiResponse.java
@@ -0,0 +1,13 @@
+package com.example.wanted.be31.global.response;
+
+import jakarta.annotation.Nullable;
+
+public record ApiResponse<T>(
+        boolean success,
+        @Nullable T data,
+        String message
+) {
+    public static <T> ApiResponse<T> success(T data) {
+        return new ApiResponse<>(true, data, "요청이 성공적으로 처리되었습니다.");
+    }
+}
